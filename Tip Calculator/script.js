0bc3f7588fce088e0e4e4611b46a15af71c4aa85
woojinho <129517591+jinho-22@users.noreply.github.com>
2023-11-20 08:34:46 +0900

    Add files via upload

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -1,18 +1,31 @@
-const btnEl = document.getElementById("calculate");
+const sliders = document.querySelectorAll("input[type='range']");
+sliders.forEach(function (slider) {
+    slider.addEventListener("input", calculateTip);
+});
+
 const billInput = document.getElementById("bill");
-const tipInput = document.getElementById("tip");
-const totalSpan = document.getElementById("total");
+billInput.addEventListener("change", calculateTip);
 
+function calculateTip() {
+    let bill = parseFloat(billInput.value);
+    let tipPercent = document.getElementById("tip").value;
+    let noOfPeople = document.getElementById("no-of-people").value;
 
-function calculateTotal() {
-    const billValue = billInput.value;
-    const tipValue = tipInput.value;
-    const totalValue = billValue * (1 + tipValue / 100);
-    totalSpan.innerText = totalValue.toFixed(2);
+    billInput.value = bill.toFixed(2);
 
-    // console.log(billValue);
-    // console.log(tipValue);
+    let totalTip = parseFloat((bill * (tipPercent / 100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+    let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
+    let totalPerPerson = (total / noOfPeople).toFixed(2);
+
+    document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
+    document.getElementById("total-amount").textContent = `\$ ${total}`;
+
+    document.getElementById("tip-percent").textContent = `\$ ${tipPercent}%`;
+    document.getElementById("split-num").textContent = noOfPeople;
+
+    document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}%`;
+    document.getElementById("total-per-person").textContent = `\$ ${totalPerPerson}%`;
 }
 
-
-btnEl.addEventListener("click", calculateTotal);
+calculateTip();
